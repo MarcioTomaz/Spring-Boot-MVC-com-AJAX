cb8193d552d0578a5ee2e33528a6574b76ee31e7
Marcio Tomaz <marciotmaz@gmail.com>
2021-05-26 16:03:35 -0300

    Capturando status error 404 e 500

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -14,10 +14,19 @@ $("#linkPromocao").on('change', function(){
 				console.log(data);
 				
 				$("#titulo").val(data.title);
-				$("#site").text(data.site.replace("@", ""));
-				$("#linkImagem").attr("src", data.image);// 1 parametro qual atributo da tag , 2 valor a adicionar
-				
-			}
+				$("#site").text(data.site.replace("@", ));
+				$("#linkImagem").attr("src", data.image);// 1 parametro qual atributo da tag , 2 valor a adicionar				
+			},
+			statusCode:{				
+				404: function(){
+					$("#alert").addClass("alert alert-danger")
+						.text("Nenhuma informação pode ser recuperada dessa URL");
+				}
+			},
+			error: function(){
+					$("#alert").addClass("alert alert-danger")
+						.text("Ops... algo deu errado, tente mais tarde");
+				}			
 		});
 		
 	}

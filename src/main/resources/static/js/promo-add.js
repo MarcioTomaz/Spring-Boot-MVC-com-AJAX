b8114234db56d38791f90a3a1dff779b3ed4776f
Marcio Tomaz <marciotmaz@gmail.com>
2021-05-27 12:17:30 -0300

    Limpando os dados da solicitação anterior

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -10,9 +10,16 @@ $("#linkPromocao").on('change', function(){
 			method:"POST",
 			url: "/meta/info?url=" + url,
 			cache: false,
+			beforeSend: function(){
+				$("#alert").removeClass("alert alert-danger").text('');
+				$("#titulo").val("");
+				$("#site").text("");
+				$("#linkImagem").attr("src", "http://localhost:8080/images/promo-dark.png");
+			},
 			success: function( data ){
 				console.log(data);
 				
+											
 				$("#titulo").val(data.title);
 				$("#site").text(data.site.replace("@", ));
 				$("#linkImagem").attr("src", data.image);// 1 parametro qual atributo da tag , 2 valor a adicionar				

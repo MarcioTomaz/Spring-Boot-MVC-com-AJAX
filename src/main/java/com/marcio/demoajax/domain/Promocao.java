4a4c2ee03a8e9a548aad578e158d389744cc2e0e
Marcio Tomaz <marciotmaz@gmail.com>
2021-05-27 15:35:48 -0300

    fix entidade e criando camada de persistencia

--- src/main/java/com/marcio/demoajax/domain/Promocao.java
+++ src/main/java/com/marcio/demoajax/domain/Promocao.java
@@ -4,37 +4,21 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import org.springframework.format.annotation.NumberFormat;
 import org.springframework.format.annotation.NumberFormat.Style;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-
-@Getter
-@Setter
-@ToString
 @SuppressWarnings("serial")
 @Entity
 @Table(name = "promocoes")
-public class Promocao implements Serializable{
-		
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+public class Promocao implements Serializable {
 
-	@Column(name = "titulo",nullable = false)
-	private String titulo;	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "titulo", nullable = false)
+	private String titulo;
 	
 	@Column(name = "link_promocao", nullable = false)
 	private String linkPromocao;
@@ -49,10 +33,10 @@ public class Promocao implements Serializable{
 	private String linkImagem;
 	
 	@NumberFormat(style = Style.CURRENCY, pattern = "#,##0.00")
-	@Column(name = "preco_produto", nullable = false)
+	@Column(name = "preco_promocao", nullable = false)
 	private BigDecimal preco;
 	
-	@Column(name = "total_likes", nullable = false)
+	@Column(name = "total_likes")
 	private int likes;
 	
 	@Column(name = "data_cadastro", nullable = false)
@@ -61,5 +45,91 @@ public class Promocao implements Serializable{
 	@ManyToOne
 	@JoinColumn(name = "categoria_fk")
 	private Categoria categoria;
-}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getLinkPromocao() {
+		return linkPromocao;
+	}
+
+	public void setLinkPromocao(String linkPromocao) {
+		this.linkPromocao = linkPromocao;
+	}
+
+	public String getSite() {
+		return site;
+	}
+
+	public void setSite(String site) {
+		this.site = site;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getLinkImagem() {
+		return linkImagem;
+	}
+
+	public void setLinkImagem(String linkImagem) {
+		this.linkImagem = linkImagem;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public int getLikes() {
+		return likes;
+	}
+
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+
+	public LocalDateTime getDtCadastro() {
+		return dtCadastro;
+	}
+
+	public void setDtCadastro(LocalDateTime dtCadastro) {
+		this.dtCadastro = dtCadastro;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	@Override
+	public String toString() {
+		return "Promocao [id=" + id + ", titulo=" + titulo + ", linkPromocao=" + linkPromocao + ", site=" + site
+				+ ", descricao=" + descricao + ", linkImagem=" + linkImagem + ", preco=" + preco + ", likes=" + likes
+				+ ", dtCadastro=" + dtCadastro + ", categoria=" + categoria + "]";
+	}	
+}
---
+++ src/main/java/com/marcio/demoajax/repository/PromocaoRepository.java
@@ -0,0 +1,9 @@
+package com.marcio.demoajax.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.marcio.demoajax.domain.Promocao;
+
+public interface PromocaoRepository extends JpaRepository<Promocao, Long>{
+
+}

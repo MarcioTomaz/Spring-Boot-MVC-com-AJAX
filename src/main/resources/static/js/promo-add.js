8cb6dc7d31c53a099be5ef9b7e2ab82507f9c079
Marcio Tomaz <marciotmaz@gmail.com>
2021-05-28 10:18:24 -0300

    funcao ajax form

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -1,4 +1,41 @@
 
+
+//submit do formulário para o controller
+$("#form-add-promo").submit(function(evento){
+	//bloquear o comportamento padrao do submit
+	evento.preventDefault();
+	
+	var promo = {};
+	promo.linkPromocao = $("#linkPromocao").val();
+	promo.descricao = $("#descricao").val();
+	promo.preco = $("#preco").val();
+	promo.titulo = $("#titulo").val();
+	promo.categoria = $("#categoria").val();
+	promo.linkImagem = $("#linkImagem").attr("src");
+	promo.site = $("#site").text();
+	
+	console.log("promo", promo);
+	
+	$.ajax({
+		method:"POST",
+		url: "/promocao/save",
+		data: promo,
+		success: function(){
+			
+			$("#alert").addClass("alert alert-success").text("Ok! Promoção cadastrada com sucesso.");
+		},
+		error: function(xhr){			
+			console.log("> error: ",xhr.responseText);
+			
+			$("alert").addClass("alert alert-danger").text("Não foi possível salvar esta promoção.");			
+		}
+	})		
+	
+		
+});
+
+
+
 //funcao para capturar as meta tags
 $("#linkPromocao").on('change', function(){
 	

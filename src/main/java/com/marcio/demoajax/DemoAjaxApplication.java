11480e7bf3637ea8126b04bc75d73a9bff05f7c6
Marcio Tomaz <marciotmaz@gmail.com>
2021-05-28 11:04:39 -0300

    método salvar promocao e fix valor monetario

--- src/main/java/com/marcio/demoajax/DemoAjaxApplication.java
+++ src/main/java/com/marcio/demoajax/DemoAjaxApplication.java
@@ -1,9 +1,14 @@
 package com.marcio.demoajax;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
+
+import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import com.marcio.demoajax.domain.SocialMetaTag;
 import com.marcio.demoajax.service.SocialMetaTagService;
@@ -12,7 +17,13 @@ import com.marcio.demoajax.service.SocialMetaTagService;
 public class DemoAjaxApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
+		
+		
 		SpringApplication.run(DemoAjaxApplication.class, args);
+	}
+	@Bean
+	public LocaleResolver localeResolver(){
+	   return new FixedLocaleResolver(new Locale("pt", "BR"));
 	}
 	
 	@Autowired
--- src/main/java/com/marcio/demoajax/web/controller/PromocaoController.java
+++ src/main/java/com/marcio/demoajax/web/controller/PromocaoController.java
@@ -1,32 +1,52 @@
 package com.marcio.demoajax.web.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.marcio.demoajax.domain.Categoria;
+import com.marcio.demoajax.domain.Promocao;
 import com.marcio.demoajax.repository.CategoriaRepository;
+import com.marcio.demoajax.repository.PromocaoRepository;
 
 @Controller
 @RequestMapping("/promocao")
 public class PromocaoController {
-
+	
+	private static Logger log = LoggerFactory.getLogger(PromocaoController.class);
+	
+    @Autowired
+    private PromocaoRepository promocaoRepository;
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
-	@ModelAttribute("categorias")
-	public List<Categoria> getCategorias(){
+	@PostMapping("/save")
+	public ResponseEntity<Promocao> salvarPromocao(Promocao promocao) {
 		
-		return categoriaRepository.findAll();
+		log.info("Promocao {}", promocao.toString());
+		promocao.setDtCadastro(LocalDateTime.now());
+		promocaoRepository.save(promocao);
+		return ResponseEntity.ok().build();
 	}
 	
+	@ModelAttribute("categorias")
+	public List<Categoria> getCategorias() {
+		
+		return categoriaRepository.findAll(); 
+	}
+
 	@GetMapping("/add")
 	public String abrirCadastro() {
+		
 		return "promo-add";
 	}
-	
 }

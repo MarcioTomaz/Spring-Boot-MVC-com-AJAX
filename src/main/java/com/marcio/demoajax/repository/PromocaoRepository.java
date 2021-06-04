0bf323f3ee3627b995b75856e09c1aa1b43b669f
Marcio Tomaz <marciotmaz@gmail.com>
2021-06-04 20:04:13 -0300

    metodos update e consulta para likes

--- src/main/java/com/marcio/demoajax/repository/PromocaoRepository.java
+++ src/main/java/com/marcio/demoajax/repository/PromocaoRepository.java
@@ -1,9 +1,21 @@
 package com.marcio.demoajax.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.marcio.demoajax.domain.Promocao;
 
 public interface PromocaoRepository extends JpaRepository<Promocao, Long>{
-
+	
+	
+	@Transactional(readOnly = false)
+	@Modifying
+	@Query("update Promocao p set p.likes = p.likes + 1 where p.id = :id")
+	void updateSomarLikes(@Param("id") Long id);
+	
+	@Query("SELECT p.likes FROM Promocao p WHERE p.id = :id") // :id é o id do parametro
+	int findLikesById(@Param("id") Long id );
 }

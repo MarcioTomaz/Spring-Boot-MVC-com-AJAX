78c29d1cd7b9b091d79736a6c21334c7c62798e7
Marcio Tomaz <marciotmaz@gmail.com>
2021-06-04 21:05:32 -0300

    Função Ajax pelo nome do site

--- src/main/resources/static/js/promo-list.js
+++ src/main/resources/static/js/promo-list.js
@@ -75,6 +75,35 @@ $("#autocomplete-input").autocomplete({
 	}
 });
 
+$("#autocomplete-submit").on("click",function(){
+	var site = $("#autocomplete-input").val();
+	
+	$.ajax({
+		method:"GET",
+		url: "/promocao/site/list",
+		data: {
+			site: site
+		},
+		beforeSend: function(){
+			pageNumber = 0;
+			$("#fim-btn").hide();
+			$(".row").fadeOut(400, function(){
+				$(this).empty();
+			});
+		},
+		success: function(response){
+			$(".row").fadeIn(250,function(){
+				$(this).append(response);
+			});
+		},
+		error: function(xhr){
+			alert("Ops, algo deu errado: " + xhr.status + ", " + xhr.statusText);
+		}
+	});
+});
+
+
+
 // Adicionar likes | espera um clique em qualquer botao q tenha likes-btn-
 $(document).on("click", "button[id*='likes-btn-']", function(){
 	var id = $(this).attr("id").split("-")[2];
